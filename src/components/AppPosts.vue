978352be01923450189c14ee298637bd255e4af7
Nikola <nikolaviki@hotmail.com>
2020-03-15 15:17:10 +0100

    Zadatak 1

---
+++ src/components/AppPosts.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Post App</div>
+</template>
+<script>
+export default {};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/posts">Posts</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
